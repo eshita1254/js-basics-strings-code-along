108ac38daef6d2164f518d352b8633b868709bc4
eshita <eshitachakrabarti9@gmail.com>
2021-12-23 22:07:31 +1100

    Done with strings

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code in this file!
+/*const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ';*/
+
+/*const currentUser = 'Grace Hopper'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`*/
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;// ['W', 'e', 'l', 'c']
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
